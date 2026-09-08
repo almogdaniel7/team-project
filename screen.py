b32539905967a2a04ba85aff6bbdc3b6994d2562
eliran <eliran.2k8@gmail.com>
2026-09-08 11:31:57 +0300

    hi!

--- screen.py
+++ screen.py
@@ -8,14 +8,13 @@ def draw_screen(field):
     bg = pygame.image.load(BACKGROUND)
     bg = pygame.transform.scale(bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
     game_display.blit(bg)
-    pygame.display.flip()
     draw_bushes(field)
+    pygame.display.flip()
 
 def draw_bushes(field):
     for i in range (BOARD_ROWS):
         for j in range (BOARD_COLS):
             if field[i][j] == BUSH_SQUARE or field[i][j] == BUSH_N_MINE_SQUARE:
                 bush = pygame.image.load(GRASS)
-                bush = pygame.transform.scale(bush, (BUSH_ROWS, BUSH_COLS))
-                game_display.blit(bush)
-                pygame.display.flip()
+                bush = pygame.transform.scale(bush, (BUSH_ROWS*CELL_SIZE, BUSH_COLS*CELL_SIZE))
+                game_display.blit(bush, bush.get_rect(center=(j*CELL_SIZE, i*CELL_SIZE)))
