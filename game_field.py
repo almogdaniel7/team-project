91dbf664be0f97603bcfe32c7a69d593f4dc08df
NevoRaz1 <nevoraz07@gmail.com>
2026-09-08 11:44:57 +0300

    Added documentation and contact function for mines

--- game_field.py
+++ game_field.py
@@ -5,6 +5,11 @@ import consts
 field = []
 
 def create():
+    """
+    Function that initializes the game field with empty squares and then adds
+    the mines and bushes to it
+    :return:
+    """
     for i in range(consts.BOARD_ROWS):
         row = []
         for j in range(consts.BOARD_COLS):
@@ -15,6 +20,10 @@ def create():
 
 
 def add_mines():
+    """
+    Function that randomly adds 20 mines around the field
+    :return:
+    """
     for i in range(consts.MINES_COUNT):
         row = random.randint(0, consts.BOARD_ROWS - 1)
         col = random.randint(0, consts.BOARD_COLS - 1)
@@ -26,6 +35,10 @@ def add_mines():
 
 
 def add_bushes():
+    """
+    Function that randomly adds 20 bushes around the field
+    :return: None
+    """
     for i in range(consts.MINES_COUNT):
         row = random.randint(0, consts.BOARD_ROWS - 1)
         col = random.randint(0, consts.BOARD_COLS - 1)
@@ -41,18 +54,37 @@ def add_bushes():
 def flag_contact(coordinates):
     """
     Returns True if at least one of the coordinates int the list touches the flag
-    :param coordinates:
-    :return:
+    :param coordinates: coordinates to check for contact with the flag (tuple(int))
+    :return: True if at least one of the coordinates int the list touches the flag (boolean)
     """
     for coordinate in coordinates:
-        if (coordinates[0] >= consts.BOARD_ROWS - consts.FLAG_ROWS - 1
-                or coordinates[1] >= consts.BOARD_COLS - consts.FLAG_COLS - 1):
+        if (coordinate[0] >= consts.BOARD_ROWS - consts.FLAG_ROWS - 1
+                or coordinate[1] >= consts.BOARD_COLS - consts.FLAG_COLS - 1):
             return True
     return False
 
 
+def mine_contact(coordinates):
+    """
+    Returns true if at least one of the coordinates int the list touches a mine
+    :param coordinates: coordinates to check for contact with the mine (tuple(int))
+    :return: True if at least one of the coordinates int the list touches a mine (boolean)
+    """
+    for i in range(consts.BOARD_ROWS):
+        for j in range(consts.BOARD_COLS):
+            if field[i][j] == consts.MINE_SQUARE or field[i][j] == consts.BUSH_N_MINE_SQUARE:
+                for coordinate in coordinates:
+                    if coordinate[0] == i and coordinate[1] == j:
+                        return True
+    return False
+
 
 def print_field():
+    """
+    Prints the field on the screen
+    TO BE DELETED LATER
+    :return:
+    """
     for i in range(consts.BOARD_ROWS):
         for j in range(consts.BOARD_COLS):
             print(field[i][j], end='\t')
