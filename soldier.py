735075e592727e9e09d709171d00da1c14b5d8a4
NevoRaz1 <nevoraz07@gmail.com>
2026-09-08 14:40:36 +0300

    hi

--- soldier.py
+++ soldier.py
@@ -1,9 +1,31 @@
+import consts
+
+position = {}
+
+def create():
+    position['x'] = 0
+    position['y'] = 0
 
 def move(direction):
-    pass
+    if direction == consts.MOVE_UP and position['y'] > 0:
+        position['y'] -= 1
+    elif direction == consts.MOVE_DOWN and position['y'] < consts.BOARD_ROWS - 1:
+        position['y'] += 1
+    elif direction == consts.MOVE_LEFT and  position['x'] > 0:
+        position['x'] -= 1
+    elif direction == consts.MOVE_RIGHT and position['x'] < consts.BOARD_COLS - 1:
+        position['x'] += 1
 
 def get_upper_body():
-    pass
+    coordinates = []
+    for row in range(consts.SOLDIER_BODY_ROWS):
+        for col in range(consts.SOLDIER_COLS):
+            coordinates.append((position['x'] + row, position['y'] + col))
+    return coordinates
 
 def get_feet():
-    pass
+    coordinates = []
+    for row in range(consts.SOLDIER_FEET_ROWS):
+        for col in range(consts.SOLDIER_COLS):
+            coordinates.append((position['x'] + row, position['y'] + col))
+    return coordinates
